363b07b54ed38a08b6a8a4513a149f1b01c0e00c
Javier Lete <jglete@eskura21.com>
2019-06-18 20:45:48 +0200

    Condicionales, y tipos en JavaScript

--- JAVASCRIPT/js/index.js
+++ JAVASCRIPT/js/index.js
@@ -1,5 +1,31 @@
+/*eslint-env browser */
 /*jslint browser:true devel:true */
+
+'use strict';
 
 //alert('Hola a todos');
 
-prompt('¿Cómo te llamas','Dime tu nombre');
+var nombre;
+
+nombre = prompt('¿Cómo te llamas?', 'Dime tu nombre');
+
+if(nombre === 'Javier Lete'){
+    alert('Hola profesor');
+} else {
+    alert('Hola ' + nombre);
+}
+
+var mes = parseInt(prompt('Dime un número de mes'));
+
+var dias;
+
+switch(mes){
+    case 2: dias = 28; break;
+    case 4:
+    case 6:
+    case 9:
+    case 11: dias = 30; break;
+    default: dias = 31;
+}
+
+alert('El mes ' + mes + ' tiene ' + dias + ' días');
