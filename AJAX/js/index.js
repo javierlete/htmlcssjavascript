468bf5de1e97265ecb30d332c841eabdc839d0e4
Javier Lete <jglete@eskura21.com>
2019-06-24 19:15:37 +0200

    Ejemplo películas con click y texto de la película

--- AJAX/js/index.js
+++ AJAX/js/index.js
@@ -3,16 +3,29 @@
 /*jslint browser:true devel:true */
 /*global $*/
 
-$(function() {
-   $.getJSON('https://swapi.co/api/films', function(resultado) {
-       console.log(resultado);
-       
-       var peliculas = resultado.results;
-       
-       console.log(peliculas);
-       
-       $(peliculas).each(function() {
-          $('#peliculas').append('<li>' + this.title + '</li>'); 
-       });
-   });
+$(function () {
+    'use strict';
+
+    $.getJSON('https://swapi.co/api/films', function (resultado) {
+        console.log(resultado);
+
+        var peliculas = resultado.results;
+
+        console.log(peliculas);
+
+        $(peliculas).each(function () {
+
+            $('#peliculas').append('<li><a href="#" data-url="' + this.url + '">' + this.title + '</a></li>');
+        });
+
+        $('#peliculas a').click(function (e) {
+            e.preventDefault();
+
+            $.getJSON(this.dataset.url, function (pelicula) {
+                console.log(pelicula.opening_crawl);
+                
+                $('#apertura').text(pelicula.opening_crawl);
+            });
+        });
+    });
 });
